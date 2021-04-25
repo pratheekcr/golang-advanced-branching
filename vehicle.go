f96b569ba5cc7772212559849072d71b8ce06782
Pratheek c Rajashekar <prajashekar@expediagroup.com>
2021-04-25 16:31:18 -0700

    Completed module

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,39 @@
 package main
 
-import (
 
-)
+type vehicle interface {
 
+}
+
+type car struct {
+	model, make, typeVehicle string
+}
+
+type truck struct {
+	model, make, typeVehicle string
+}
+
+type bike struct {
+	model, make string
+}
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model "json:\"values\""
+}
 
 // Model array for the feedback.json file
+type Model struct{
+	Name string "json:\"model\""
+	Feedback []string "json:\"feedback\""
+}
+
+type FeedbackResult struct {
+	feedbackTotal, feedbackPositive, feedbackNegative, feedbackNeutral int
+}
+
+var vehicleResult map[string]FeedbackResult
+var inventory []vehicle
 
 type rating float32
 
@@ -21,7 +47,7 @@ const (
 
 func init() {
 
-/*
+
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,8 +60,8 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]FeedbackResult)
 
 }
 
